18eb0b9a8b042e476cd4e19e3a191b3a566f8b7c
samHuynh-803 <anhkhoahuynh803@gmail.com>
2025-09-02 22:40:36 -0400

    Comment Change Version 2

--- ver2/Account.java
+++ ver2/Account.java
@@ -2,7 +2,7 @@ package ver2;
 
 public class Account {
 	// Instance variable, only available inside this class.
-		private double balance;
+		private double balance;//Comment for Balance.
 		private String name;
 		public String getName() {
 			return name;
